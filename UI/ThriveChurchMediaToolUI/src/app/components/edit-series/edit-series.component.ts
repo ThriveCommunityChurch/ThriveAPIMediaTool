a610b54c0fdc0b3213bc2d321baea25ab67a7f1c
Wyatt Baggett <wyattbaggett@yahoo.com>
2022-01-09 18:00:39 -0500

    Adding new message to series works as expected

--- UI/ThriveChurchMediaToolUI/src/app/components/edit-series/edit-series.component.ts
+++ UI/ThriveChurchMediaToolUI/src/app/components/edit-series/edit-series.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { AddMessagesToSeriesRequest } from 'src/app/DTO/AddMessagesToSeriesRequest';
+import { SermonMessageRequest } from 'src/app/DTO/SermonMessageRequest';
 import { SermonSeries } from 'src/app/DTO/SermonSeries';
 import { ApiService } from 'src/app/services/api-service.service';
 
@@ -13,6 +15,10 @@ export class EditSeriesComponent implements OnInit {
   seriesId: string | null = null;
   seriesName: string = "N/A";
   sermonSeries: SermonSeries | null = null;
+
+  // Item form
+  submitButtonMessage = "Add item";
+  cancelButtonMessage = "Cancel adding item";
 
   constructor(
     private route: ActivatedRoute,
@@ -38,6 +44,40 @@ export class EditSeriesComponent implements OnInit {
         }
       });
     }
+  }
+
+ /**
+ * Method to add new Series Media.
+ * @param item - `SeriesMedia`: to create
+ */
+  addItemEventHandler(item: SermonMessageRequest): void {
+    if (item && this.seriesId) {
+
+      let tempItems: SermonMessageRequest[] = [item];
+      let request: AddMessagesToSeriesRequest = {
+        MessagesToAdd: tempItems
+      }
+
+      this.apiService.addMessageToSeries(this.seriesId, request)
+      // clone the data object, using its known Config shape
+      .subscribe(resp => {
+        // display its headers
+
+        if (resp.status > 200) {
+          console.log(resp.body);
+        }
+        else if (resp.body) {
+          alert("Success!");
+        }
+      });
+    }
+  }
+
+  /**
+  * Handler method to close AddItem sub form from the child component.
+  */
+  cancelAddItemEventHandler(): void {
+    // do nothing
   }
 
 }
--- UI/ThriveChurchMediaToolUI/src/app/services/api-service.service.ts
+++ UI/ThriveChurchMediaToolUI/src/app/services/api-service.service.ts
@@ -1,11 +1,11 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders, HttpResponse } from '@angular/common/http';
-import { Observable, throwError } from 'rxjs';
-import { catchError, retry } from 'rxjs/operators';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Observable } from 'rxjs';
 import { SermonSummaryResponse } from '../DTO/SermonSummaryResponse';
 import { Configurations } from '../shared/configurations';
 import { CreateSermonSeriesRequest } from '../DTO/CreateSermonSeriesRequest';
 import { SermonSeries } from '../DTO/SermonSeries';
+import { AddMessagesToSeriesRequest } from '../DTO/AddMessagesToSeriesRequest';
 
 @Injectable()
 export class ApiService {
@@ -39,6 +39,16 @@ export class ApiService {
   getSeries(id: string): Observable<HttpResponse<SermonSeries>> {
     return this.http.get<SermonSeries>(
       this.apiUrl.concat(`/api/sermons/series/${id}`),
+      {
+        observe: 'response'
+      }
+    )
+  }
+
+  addMessageToSeries(seriesId: string, newMessage: AddMessagesToSeriesRequest): Observable<HttpResponse<SermonSeries>> {
+    return this.http.post<SermonSeries>(
+      this.apiUrl.concat(`/api/sermons/series/${seriesId}/message`),
+      newMessage,
       {
         observe: 'response'
       }
