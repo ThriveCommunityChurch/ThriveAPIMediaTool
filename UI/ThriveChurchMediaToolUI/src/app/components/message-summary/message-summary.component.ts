199221038dad418d623c0150c4414da2d80e56bf
Wyatt Baggett <wyattbaggett@yahoo.com>
2023-10-13 19:40:48 -0400

    New view messages page & component

---
+++ UI/ThriveChurchMediaToolUI/src/app/components/message-summary/message-summary.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-message-summary',
+  templateUrl: './message-summary.component.html',
+  styleUrls: ['./message-summary.component.scss']
+})
+export class MessageSummaryComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ UI/ThriveChurchMediaToolUI/src/app/components/view-series/view-series.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SermonSeries } from 'src/app/DTO/SermonSeries';
+import { ApiService } from 'src/app/services/api-service.service';
+
+@Component({
+  selector: 'app-view-series',
+  templateUrl: './view-series.component.html',
+  styleUrls: ['./view-series.component.scss']
+})
+export class ViewSeriesComponent implements OnInit {
+
+  seriesId: string | null = null;
+  seriesName: string = "N/A";
+  sermonSeries: SermonSeries;
+
+  constructor(
+    private route: ActivatedRoute,
+    private _router: Router,
+    private apiService: ApiService
+  ) { }
+
+  ngOnInit(): void {
+    this.seriesId = this.route.snapshot.paramMap.get('id');
+    
+    if (this.seriesId) {
+      this.apiService.getSeries(this.seriesId)
+      // clone the data object, using its known Config shape
+      .subscribe(resp => {
+        // display its headers
+
+        if (resp.status > 200) {
+          console.log(resp.body);
+        }
+        else if (resp.body) {
+          this.sermonSeries = resp.body;
+        }
+      });
+    }
+  }
+
+}
